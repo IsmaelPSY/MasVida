a7188566be2177dace2d261495a65ec49c87466a
IsmaelPSY <ismaelpsy.12@gmail.com>
2021-02-05 19:46:19 -0500

    Primer cambio

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+//Bootstrap
+import Tab from 'react-bootstrap/Tab';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+//Componentes
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+  <Row>
+    <Col sm={3}>
+      <Nav variant="pills" className="flex-column">
+        <Nav.Item>
+          <Nav.Link eventKey="first">MAS VIDA</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="second">Soy un Centro de Salud</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="third">Quiero una Cita Médica</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="fourth">Realizar Diagnóstico</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </Col>
+    <Col sm={9}>
+      <Tab.Content>
+        <Tab.Pane eventKey="first">
+          <Home/>
+        </Tab.Pane>
+        <Tab.Pane eventKey="second">
+         
+        </Tab.Pane>
+      </Tab.Content>
+    </Col>
+  </Row>
+</Tab.Container>
     </div>
   );
 }
---
+++ my-app/src/components/Home/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../../assets/css/Home.css';
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+
+import logo from '../../assets/images/Liz.png';
+
+
+
+class Home extends React.Component{
+
+  render(){
+    return(
+    <div className="Home">
+      <React.Fragment >
+        <h1><span>♥</span><span>M</span><span>A</span><span>S</span><span>♥</span><span>V</span><span>I</span><span>D</span><span>A</span><span>♥</span></h1>
+        <Container>
+        <Image className="logo" src={logo}/>
+        </Container>
+      </React.Fragment>
+      </div>
+    );
+  }
+
+}
+
+export default Home;
